570cc6bb1789e8e5936867e7841ecde11b81d87a
tamaslud <tamaslud@yahoo.com>
2022-08-10 21:50:25 +0200

    test1 - arrow function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const allButtons = document.querySelectorAll('.button');
+/*
 
 function handleClick() {
     for (let i = 0; i < allButtons.length; i += 1) {
@@ -10,5 +11,35 @@ function handleClick() {
       }
 };
 
+const handleClick = function() {
+    for (let i = 0; i < allButtons.length; i += 1) {
+        allButtons[i].addEventListener('click', function () {
+          console.log(allButtons[i].innerHTML);
+        })
+      }
+};
 
+const handleClick = () => {
+    for (let i = 0; i < allButtons.length; i += 1) {
+        allButtons[i].addEventListener('click', function () {
+            console.log(allButtons[i].innerHTML);
+        })
+    }
+};
+
+const handleClick = () => {
+    for (let i = 0; i < allButtons.length; i += 1) {
+        allButtons[i].addEventListener('click', () => {
+            console.log(allButtons[i].innerHTML);
+        })
+    }
+};
+
+*/
+
+const handleClick = () => {
+    for (let i = 0; i < allButtons.length; i += 1) {
+        allButtons[i].addEventListener('click', () => console.log(allButtons[i].innerHTML))
+    }
+};
 

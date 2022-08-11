51887386e7c8c05c0261f600fc0566166bd8970e
tamaslud <tamaslud@yahoo.com>
2022-08-11 08:44:57 +0200

    correction

--- js/person.js
+++ js/person.js
@@ -1,6 +1,6 @@
 'use strict';
 
-defaultPerson = {firstname : 'John', lastname : 'Doe', age : 33}
+const defaultPerson = {firstname : 'John', lastname : 'Doe', age : 33};
 
 const personDataLog = (person = {firstname : 'John', lastname : 'Doe', age : 33}) => {
     return `My name is ` + person.firstname + ` ` + person.lastname+`. I'm ` 
